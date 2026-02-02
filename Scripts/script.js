1ccdd96598a4da1911803579f301c8e4649bf55d
James Caldwell <james.c103003@gmail.com>
2026-02-01 20:35:58 -0500

    updated javascript
    added random color to cells and opacity

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,15 +12,31 @@ function createGrid(cellAmount) {
         cell.style.height = `${size}px`;
         cell.classList.add("cell");
         cell.style.outline = '1px solid black'
+        cell.dataset.opacity = 0
         
         cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = 'green'
+            let opacity = Number(cell.dataset.opacity)
+
+            if ( opacity === 0){
+                cell.style.backgroundColor = getRandomColor()
+                opacity += 0.1
+            }
+            else {
+                opacity += 0.1
+                if (opacity > 1) opacity = 1;
+            }
+            cell.dataset.opacity = opacity
+            cell.style.opacity = opacity
         })
         gridContainer.appendChild(cell)
 
     }
 }
 
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
+    return `#${randomColor}`;
+}
 
 createGrid(cellAmount)
 
